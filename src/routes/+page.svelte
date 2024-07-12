07d9c533c94b0c373138f1913336fa0655d98afc
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2024-07-11 19:42:57 -0700

    Squashed commit of the following:
    commit 8811302e44811ccb2145027eea0bc1b9182ba30c
    Author: EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
    Date:   Thu Jul 11 19:42:08 2024 -0700
    
        add greeting component
    
    commit ec3acaee7fa49651090f2564084565204b69354e
    Author: EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
    Date:   Thu Jul 11 19:33:40 2024 -0700
    
        initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script lang="ts">
+	import Greeting from '$lib/components/greeting.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Greeting name="Open Source Software team" />
